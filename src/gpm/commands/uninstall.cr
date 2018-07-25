d83eb208a5435d600965e0850a0e113838e20777
cbrnrd <cbawsome77@gmail.com>
2018-07-25 19:31:03 -0400

    Fix bug in uninstall

--- src/gpm/commands/uninstall.cr
+++ src/gpm/commands/uninstall.cr
@@ -6,7 +6,7 @@ class Uninstall
   def self.uninstall(repo : String)
     print "Removing all bins for repo: #{repo}..."
     bins = get_repo_bins(repo) # If this fails, it'll exit, so no check here
-    bins.each do |b|
+    bins.as_a.each do |b|
       print "  Removing #{b}..."
       rm_rf(b.as_s)
       done_msg
